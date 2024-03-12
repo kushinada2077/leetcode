1247cf1e4c8335b47f56923cdd3e054c7c221d81
kushinada <111179843+movingsummer@users.noreply.github.com>
2024-03-12 10:02:43 +0900

    Time: 8 ms (59.04%), Space: 8.3 MB (42.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
 
         int cpy = x;
         int rev = 0;
 
-        while (cpy > 1 && cpy > rev) {
+        while (cpy > rev) {
             rev = rev * 10 + cpy % 10;
             cpy /= 10;
         }
