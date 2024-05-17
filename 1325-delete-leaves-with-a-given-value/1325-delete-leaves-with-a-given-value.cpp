9ec723bf06013f8d2456046e39ccb26cff44850e
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-05-17 11:12:04 +0900

    Time: 12 ms (47.76%), Space: 21.2 MB (47.43%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -11,30 +11,15 @@
  * };
  */
 class Solution {
-    unordered_map<TreeNode*, TreeNode*> p;
-
 public:
-    void sol(TreeNode* cur, int target) {
-        if (cur->left) {
-            p[cur->left] = cur;
-            sol(cur->left, target);
-        }
-        if (cur->right) {
-            p[cur->right] = cur;
-            sol(cur->right, target);
-        }
-        if (!cur->left && !cur->right && cur->val == target) {
-            if (p[cur]->left == cur)
-                p[cur]->left = nullptr;
-            else if (p[cur]->right == cur)
-                p[cur]->right = nullptr;
-        }
-    }
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        TreeNode* dummy = new TreeNode();
-        dummy->left = root;
-        p[root] = dummy;
-        sol(root, target);
-        return dummy->left;
+        if (!root) return NULL;
+        
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        if (!root->left && !root->right && target == root->val) return NULL;
+
+        return root;
     }
 };
