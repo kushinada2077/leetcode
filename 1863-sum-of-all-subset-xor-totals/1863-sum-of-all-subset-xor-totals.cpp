91fbef9dd351b5b2141bcd88beb04e5319a598b6
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-05-20 18:07:35 +0900

    Time: 5 ms (41.71%), Space: 8.4 MB (65.34%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,16 +1,17 @@
 class Solution {
-    int sum = 0;
 public:
-    void sol(int n, int res, vector<int>& nums) {
-        if (n == nums.size()) {
-            sum += res;
-            return;
-        }
-        sol(n + 1, res ^ nums[n], nums);
-        sol(n + 1, res, nums);
-    }
     int subsetXORSum(vector<int>& nums) {
-        sol(0, 0, nums);
-        return sum;
+        int len = nums.size(), brute = (1 << len), ans = 0;
+        for (int i = 0; i < brute; i++) {
+            int XOR = 0;
+            for (int j = 0; j < len; j++) {
+                if (i & (1 << j)) {
+                    XOR ^= nums[j];
+                }
+            }
+            ans += XOR;
+        }
+
+        return ans;
     }
 };
