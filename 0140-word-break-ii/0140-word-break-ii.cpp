d4d994fa9b8e7ee492337853d57bd43fb67c2639
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-05-25 21:55:15 +0900

    Time: 0 ms (100%), Space: 9 MB (66.81%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int n; 
+    vector<string> sen;
+    vector<string> ans;
+    set<string> dic;
+    void sol(const string& s, int k) {
+        if (n == k) {
+            string tmp = "";
+            for (auto word : sen) tmp += (word + " ");
+            tmp = tmp.substr(0, tmp.size() - 1);
+            ans.push_back(tmp);
+            return;
+        }
+
+        for (int i = 1; i <= n - k; i++) {
+            string word = s.substr(k, i);
+            if (dic.find(word) != dic.end()) {
+                sen.push_back(word);
+                sol(s, k + i);
+                sen.pop_back();
+            }
+       }
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        n = s.size();
+        for (auto word : wordDict) dic.insert(word);
+        sol(s, 0);
+        return ans;
+    }
+};
