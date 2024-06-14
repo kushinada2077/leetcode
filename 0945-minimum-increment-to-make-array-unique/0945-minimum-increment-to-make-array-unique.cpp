14aba94a27ef3987e33ffcccb2a31d118c5c3580
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-14 16:08:12 +0900

    Time: 122 ms (52.95%), Space: 69.2 MB (24.61%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        unordered_map<int, int> m;
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int ans = 0;
+        sort(nums.begin(), nums.end());
+
+        int u = 0, ans = 0;
         for (auto n : nums) {
-            m[n]++;
-            if (m[n] > 1)
-                pq.push(n);
-        }
-        int u = 1;
-        while (!pq.empty()) {
-            int n = pq.top();
-            pq.pop();
-            while (u <= n || m[u] > 0)
-                u++;
-            ans += u - n; 
-            m[u]++;
+            u = max(u, n);
+            ans += u - n;
+            u++;
         }
         return ans;
     }
