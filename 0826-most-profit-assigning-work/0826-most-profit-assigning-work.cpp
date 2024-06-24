2b844a62b00f963d32a602faefad361c19bd0982
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-25 01:11:19 +0900

    Time: 62 ms (40.81%), Space: 41.7 MB (34.34%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         int n = difficulty.size(), m = worker.size(), ans = 0;
-        vector<int> pro_idx;
-        for (int i = 0; i < n; ++i) pro_idx.push_back(i);
-        sort(pro_idx.begin(), pro_idx.end(), [profit](int a, int b) {
-            return profit[a] > profit[b];
-        });
         priority_queue<int> pq(worker.begin(), worker.end());
-        for (auto i : pro_idx) {
-            int d = difficulty[i];
+        priority_queue<pair<int, int>> job;
+        for (int i = 0; i < n; ++i) {
             int p = profit[i];
+            int d = difficulty[i];
+            job.push({p, d});
+        }
+
+        while (!job.empty() && !pq.empty()) {
+            auto [p, d] = job.top();
+            job.pop();
             while (!pq.empty() && pq.top() >= d) {
                 ans += p;
                 pq.pop();
             }
-            if (pq.empty()) break;
-        }
+        } 
         return ans;
     }
 };
