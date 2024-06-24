4fcd40aa9e9e0248a344a5c5c77a29da80c5a58d
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-24 22:22:53 +0900

    Time: 89 ms (63.93%), Space: 113.3 MB (22.34%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -3,15 +3,15 @@ public:
     int minKBitFlips(vector<int>& nums, int k) {
         int flipped = 0, ans = 0, n = nums.size();
         vector<int> isFlipped(n, 0);
-        for (int i = 0; i < n; ++i) {
-            if (i >= k) {
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i >= k)
                 flipped ^= isFlipped[i - k];
-            }
-            if (flipped == nums[i]) {
+            if (nums[i] == flipped) {
                 if (i + k > n) return -1;
-                isFlipped[i] ^= 1;
-                flipped ^= 1;
                 ans++;
+                isFlipped[i] = 1;
+                flipped ^= 1;
             }
         }
         return ans;
