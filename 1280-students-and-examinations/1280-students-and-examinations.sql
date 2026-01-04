8c80a7837084bff4178524b2fe58a3ad40a21f60
kushinada <kushinada2077@gmail.com>
2026-01-04 17:05:18 +0900

    Time: 1043 ms (21.47%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,4 +1,13 @@
-SELECT stu.student_id, stu.student_name, sub.subject_name, (SELECT COUNT(*) FROM Examinations AS e
-WHERE stu.student_id = e.student_id AND sub.subject_name = e.subject_name) AS attended_exams
-FROM Students AS stu, Subjects AS sub
+SELECT 
+    stu.student_id student_id,
+    stu.student_name student_name,
+    sub.subject_name subject_name,
+    COUNT(e.student_id) attended_exams
+FROM Students stu
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+ON 
+    stu.student_id = e.student_id
+    AND sub.subject_name = e.subject_name
+GROUP BY stu.student_id, stu.student_id, sub.subject_name
 ORDER BY stu.student_id, sub.subject_name;
