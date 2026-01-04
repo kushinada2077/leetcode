dce281ef81819130a2fa267aaa37b9ed4d40c6b6
kushinada <kushinada2077@gmail.com>
2026-01-04 16:25:00 +0900

    Time: 358 ms (85.63%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,9 @@
 SELECT w1.id id
 FROM Weather w1
-CROSS JOIN Weather w2
-WHERE
-    w2.temperature < w1.temperature
-    AND DATEDIFF(w1.recordDate, w2.recordDate) = 1;
+WHERE EXISTS (
+    SELECT 1
+    FROM Weather w2
+    WHERE 
+        DATE_ADD(w2.recordDate, INTERVAL 1 DAY) = w1.recordDate
+        AND w2.temperature < w1.temperature
+);
