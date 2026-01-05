dc005bac6b90f19494346d2d8708b863745b1fa3
kushinada <kushinada2077@gmail.com>
2026-01-05 23:01:08 +0900

    Time: 610 ms (61.67%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,10 +1,4 @@
-SELECT s.user_id, 
-        ROUND(AVG(
-                CASE
-                    WHEN c.action = 'confirmed' THEN 1
-                    ELSE 0
-                END
-        ), 2) AS confirmation_rate
+SELECT s.user_id user_id, ROUND(AVG(COALESCE(action = 'confirmed', 0)), 2) confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c ON s.user_id = c.user_id
 GROUP BY s.user_id;
