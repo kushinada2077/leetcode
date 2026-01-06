34b7e1214265a3068ae28a03f10cc57c8975d187
kushinada <kushinada2077@gmail.com>
2026-01-06 13:20:21 +0900

    Time: 459 ms (82.07%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,4 +1,7 @@
-SELECT p.project_id, ROUND(AVG(experience_years), 2) AS average_years
+SELECT
+    p.project_id project_id,
+    ROUND(AVG(e.experience_years), 2) average_years
 FROM Project p
-JOIN Employee e ON p.employee_id = e.employee_id
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
 GROUP BY p.project_id;
