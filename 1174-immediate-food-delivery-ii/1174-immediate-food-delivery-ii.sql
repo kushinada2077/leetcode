a28d25ddc39fc5dbe9352222b05f2d94de7d94bd
kushinada <kushinada2077@gmail.com>
2026-01-06 14:12:55 +0900

    Time: 668 ms (21.1%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,7 +1,8 @@
-SELECT ROUND(100.0 * AVG(CASE WHEN main.min_date = sub.customer_pref_delivery_date THEN 1 ELSE 0 END), 2) immediate_percentage
-FROM (
-SELECT customer_id, MIN(order_date) min_date
-FROM Delivery
-GROUP BY customer_id
-) main
-JOIN Delivery sub ON main.customer_id = sub.customer_id AND main.min_date = sub.order_date;
+SELECT ROUND(100 * AVG(order_date = customer_pref_delivery_date), 2) immediate_percentage
+FROM Delivery 
+WHERE (customer_id, order_date) IN 
+(
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
