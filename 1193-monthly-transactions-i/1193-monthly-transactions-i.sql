c62eaaad5a3f3d0ded795dd6fd8e5bab72f796f4
kushinada <kushinada2077@gmail.com>
2026-01-06 14:00:43 +0900

    Time: 562 ms (51.74%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,7 +1,9 @@
-SELECT DATE_FORMAT(trans_date, '%Y-%m') month, country, 
-        COUNT(*) trans_count,
-        SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) approved_count,
-        SUM(amount) trans_total_amount,
-        SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) approved_total_amount
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') month,
+    country,
+    COUNT(*) trans_count,
+    SUM(state = 'approved') approved_count,
+    SUM(amount) trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) approved_total_amount
 FROM Transactions
-GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country;
+GROUP BY month, country;
