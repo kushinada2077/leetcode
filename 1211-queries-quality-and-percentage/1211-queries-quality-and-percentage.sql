0925fcbc1596588be2176ce812e0397b5bfbd685
kushinada <kushinada2077@gmail.com>
2026-01-06 13:35:45 +0900

    Time: 319 ms (87.74%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,5 +1,6 @@
-SELECT query_name, 
-        ROUND(AVG(rating * 1.0 / position), 2) AS quality,
-        ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage
+SELECT
+    query_name,
+    ROUND(AVG(rating / position), 2) quality,
+    ROUND(100 * SUM(rating < 3) / COUNT(*), 2) poor_query_percentage
 FROM Queries
 GROUP BY query_name;
