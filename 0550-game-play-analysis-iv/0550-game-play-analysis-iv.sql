e718732aa8122dcee225fc71cda0d43559678f38
kushinada <kushinada2077@gmail.com>
2026-01-06 14:18:20 +0900

    Time: 534 ms (54.74%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,8 +1,7 @@
-SELECT ROUND(COUNT(*) * 1.0 / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) fraction
-FROM activity
-WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN
-(
-SELECT player_id, MIN(event_date) first_day
-FROM Activity
-GROUP BY player_id
+SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) fraction
+FROM Activity 
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN (
+    SELECT player_id, MIN(event_date)
+    FROM Activity
+    GROUP BY player_id
 );
