75bd2ba4a472d4c35c3cf5b1c8e577736d69cf02
kushinada <kushinada2077@gmail.com>
2026-01-19 19:03:55 +0900

    Time: 656 ms (22.9%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,15 +1,20 @@
-SELECT p1.product_id, (CASE WHEN p3.change_date IS NULL THEN 10 ELSE p3.new_price END) price
-FROM
-(
-SELECT DISTINCT product_id
-FROM Products
+SELECT
+    p1.product_id,
+    (CASE
+        WHEN MAX(p2.change_date) IS NULL
+        THEN 10
+        ELSE (
+            SELECT new_price
+            FROM Products
+            WHERE p1.product_id = Products.product_id AND MAX(p2.change_date) = Products.change_date
+        )
+        END
+    ) price  
+FROM (
+    SELECT product_id
+    FROM Products
+    GROUP BY product_id
 ) p1
-LEFT JOIN (
-    SELECT product_id, MAX(change_date) max_date
-    FROM Products p3
-    WHERE p3.change_date <= '2019-08-16'
-    GROUP BY p3.product_id
-    ) p2
-    ON p1.product_id = p2.product_id
-LEFT JOIN Products p3 
-ON p1.product_id = p3.product_id AND p2.max_date = p3.change_date
+LEFT JOIN Products p2
+ON p1.product_id = p2.product_id AND p2.change_date <= '2019-08-16'
+GROUP BY p1.product_id;
