4f4179317fdb16598272972b492c75fee57495dd
kushinada <kushinada2077@gmail.com>
2026-01-30 15:30:23 +0900

    Time: 344 ms (54.91%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,11 +1,14 @@
-SELECT c1.visited_on, SUM(c2.amount) amount, ROUND(SUM(c2.amount) * 1.0 / COUNT(DISTINCT c2.visited_on), 2) average_amount
+SELECT
+    c1.visited_on,
+    SUM(c2.amount) amount,
+    ROUND(SUM(c2.amount) / 7, 2) average_amount
 FROM (
     SELECT DISTINCT visited_on
     FROM Customer
 ) c1
-JOIN Customer c2 ON
-    DATE_ADD((SELECT MIN(visited_on) FROM Customer), INTERVAL 6 DAY) <= c1.visited_on 
-    AND DATE_SUB(c1.visited_on, INTERVAL 7 DAY) < c2.visited_on
-    AND c2.visited_on <= c1.visited_on
-GROUP BY c1.visited_on
+JOIN Customer c2
+ON c2.visited_on BETWEEN
+    DATE_SUB(c1.visited_on, INTERVAL 6 DAY) AND
+    c1.visited_on
+GROUP BY c1.visited_on HAVING DATE_SUB(MAX(c2.visited_on), INTERVAL 6 DAY) >= (SELECT MIN(visited_on) FROM Customer)
 ORDER BY c1.visited_on;
